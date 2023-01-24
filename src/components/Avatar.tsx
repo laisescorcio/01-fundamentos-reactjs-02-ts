ca75c88a6ae8ebb45382dfd186d7cf5d4cbbb667
laisescorcio <lais.correia@outlook.com>
2023-01-23 23:09:15 -0300

    feat: Avatar and Comment - applied typescript

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -1,10 +1,23 @@
 import styles from './Avatar.module.css';
 
-export function Avatar({ hasBorder = true, src }) {
+
+// Typescript: definido a tipagem do AvatarProps porque estava dando erro no 'alt' de outro componente que chamava o componente Avatar
+
+interface AvatarProps {
+    hasBorder?: boolean; // 
+    src: string;
+    alt?: string;
+}
+// Typescript: quando há o ponto ? é porque é uma propriedade opcional. Exemplo: alt?:string
+// dessa forma evita erro quando chamamos o componente Avatar em outro componente e não passamos uma dessas propriedades
+
+// Typescript: para objeto não se define o tipo da informação separadamente, por isso foi criado o AvatarProps, englobando todas essas propriedades
+export function Avatar({ hasBorder = true, src, alt }: AvatarProps) {
     return (
-        <img 
+        <img
             className={hasBorder ? styles.avatarWithBorder : styles.avatar}
-            src={src} 
+            src={src}
+            alt={alt}
         />
     )
 }
--- src/components/Comment.tsx
+++ src/components/Comment.tsx
@@ -3,10 +3,20 @@ import { useState } from 'react';
 import { Avatar } from './Avatar'
 import styles from './Comment.module.css'
 
-export function Comment({content, onDeleteComment}) {
-    const [ likeCount, setLikeCount ] = useState(0);
 
-    function handleLikeComment () {
+// Typescript: Sobre o onDeleteComment é feita uma definicao de uma propriedade que é uma função () =>
+// e é definido o return dela. Como essa função não retorna nada, é usado o void
+// e a funcao onDeleteComment recebe um parametro, que é um comentário, que por sua vez é uma string (comment: string)
+interface CommentProps {
+    content: string;
+    onDeleteComment: (comment: string) => void
+}
+
+// Typescript: para objeto não se define o tipo da informação separadamente, por isso foi criado o CommentProps, englobando todas essas propriedades
+export function Comment({ content, onDeleteComment }: CommentProps) {
+    const [likeCount, setLikeCount] = useState(0);
+
+    function handleLikeComment() {
         setLikeCount((state) => {
             return state + 1
         })
@@ -18,27 +28,27 @@ export function Comment({content, onDeleteComment}) {
 
     return (
         <div className={styles.comment}>
-            <Avatar hasBorder={false} src="https://avatars.githubusercontent.com/u/61030205?v=4"/>
-            
+            <Avatar hasBorder={false} src="https://avatars.githubusercontent.com/u/61030205?v=4" alt="" />
+
             <div className={styles.commentBox}>
                 <div className={styles.commentContent}>
                     <header>
                         <div className={styles.authorAndTime}>
                             <strong>Laís Escorcio</strong>
-                            <time title="11 de Maio às 08h13" dateTime="2022-05-11 08:13:30">Cerca de 1h atrás</time>      
+                            <time title="11 de Maio às 08h13" dateTime="2022-05-11 08:13:30">Cerca de 1h atrás</time>
                         </div>
 
-                        <button 
+                        <button
                             onClick={handleDeleteComment}
                             title="Deletar comentário">
-                                <Trash size={24} />
+                            <Trash size={24} />
                         </button>
                     </header>
                     <p>{content}</p>
                 </div>
-                
+
                 <footer>
-                    <button 
+                    <button
                         onClick={handleLikeComment}
                     >
                         <ThumbsUp />
